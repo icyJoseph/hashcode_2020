530aacf87771b2b71d52a13a15f13884f802fbc5
Joseph Chamochumbi <sephxd1234@gmail.com>
2020-02-20 17:49:18 +0100

    feat: ready to compete

--- src/index.js
+++ src/index.js
@@ -7,15 +7,23 @@ const printOut = data => {
   const outputFile = file.replace("input", "output").replace(".in", ".out");
 
   fs.writeFile(outputFile, data, err => {
-    if (err) return console.log(err);
+    if (err) return console.log("Print out: ", err);
 
     console.log("Success");
   });
 };
 
 fs.readFile(path.resolve(__dirname, "..", file), "utf-8", (err, data) => {
-  if (err) return console.log(err);
+  if (err) return console.log("Read Error: ", err);
   console.log(data);
+
+  // solve here
+
+  const [header, ...rest] = data.split("\n").filter(e => e);
+
+  console.log({ header });
+
+  console.log({ rest });
 
   printOut(data);
 });
