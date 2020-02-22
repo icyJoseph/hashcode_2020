6a719881c14d99b004fd7914314a50abdbbc5672
Joseph Chamochumbi <sephxd1234@gmail.com>
2020-02-22 09:12:16 +0100

    feat: break down library processing into multiple functions - optimize example

--- src/index.js
+++ src/index.js
@@ -3,90 +3,8 @@ const path = require("path");
 
 const [, , file] = process.argv;
 
-const printOut = data => {
+const printOut = output => {
   const outputFile = file.replace("input", "output").replace(".txt", ".out");
-
-  fs.writeFile(outputFile, data, err => {
-    if (err) return console.log("Print out: ", err);
-
-    console.log("Success");
-  });
-};
-
-fs.readFile(path.resolve(__dirname, "..", file), "utf-8", (err, data) => {
-  if (err) return console.log("Read Error: ", err);
-
-  const scannedBooks = new Set();
-
-  const [header, subheader, ...rest] = data.split("\n").filter(e => e);
-
-  const [numOfBooks, numOfLibraries, numOfDays] = header
-    .split(" ")
-    .map(e => parseInt(e));
-
-  const bookPoints = subheader.split(" ").map(e => parseInt(e));
-
-  const libraries = rest
-    .reduce((prev, curr, index, src) => {
-      if (index % 2 === 0) {
-        const [numOfLibraryBooks, signUp, shipCapacity] = curr
-          .split(" ")
-          .map(e => parseInt(e));
-
-        const libraryBooks = src[index + 1]
-          .split(" ")
-          .map(e => parseInt(e))
-          .sort((a, b) => bookPoints[b] - bookPoints[a]);
-
-        const maxPoints = libraryBooks.reduce(
-          (acc, book) => acc + bookPoints[book],
-          0
-        );
-
-        return [
-          ...prev,
-          {
-            index: index / 2,
-            libraryBooks,
-            numOfLibraryBooks,
-            signUp,
-            shipCapacity,
-            maxPoints
-          }
-        ];
-      }
-      return prev;
-    }, [])
-    .sort((a, b) => b.maxPoints - a.maxPoints);
-
-  console.log({ numOfBooks, numOfLibraries, numOfDays, bookPoints, libraries });
-
-  let daysLeft = numOfDays;
-  let output = [];
-  let i = 0;
-
-  while (daysLeft >= 0) {
-    const currLib = libraries[i];
-
-    if (!currLib) {
-      break;
-    }
-
-    const qTyOfBooksToShip = Math.floor(daysLeft / currLib.shipCapacity);
-
-    const booksToShip = currLib.libraryBooks
-      .filter(e => !scannedBooks.has(e))
-      .slice(0, qTyOfBooksToShip);
-
-    daysLeft = daysLeft - currLib.signUp;
-
-    i = i + 1;
-
-    if (booksToShip.length) {
-      booksToShip.forEach(book => scannedBooks.add(book));
-      output.push({ ...currLib, booksToShip });
-    }
-  }
 
   const rows = output
     .map(
@@ -95,7 +13,167 @@ fs.readFile(path.resolve(__dirname, "..", file), "utf-8", (err, data) => {
     )
     .join("\n");
 
-  const toPrint = [output.length, rows].join("\n");
+  const data = [output.length, rows].join("\n");
 
-  printOut(toPrint);
+  fs.writeFile(outputFile, data, err => {
+    if (err) return console.log("Print out: ", err);
+
+    console.log("Success");
+  });
+};
+
+function estimatePoints(output, bookPoints) {
+  console.log(
+    "Estimated points",
+    output.reduce(
+      (acc, curr) =>
+        acc + curr.booksToShip.reduce((acc, curr) => acc + bookPoints[curr], 0),
+      0
+    )
+  );
+}
+
+// takes "2 3 4" => [2,3,4] as number[]
+const readNumericInputLine = data => data.split(" ").map(e => parseInt(e));
+
+function parseInputData(data) {
+  const [header, subheader, ...rest] = data.split("\n").filter(e => e);
+
+  const [numOfBooks, numOfLibraries, numOfDays] = header
+    .split(" ")
+    .map(e => parseInt(e));
+
+  const bookPoints = subheader.split(" ").map(e => parseInt(e));
+
+  const libraries = rest.reduce((prev, curr, index, src) => {
+    if (index % 2 === 0) {
+      const [numOfLibraryBooks, signUp, shipCapacity] = readNumericInputLine(
+        curr
+      );
+
+      const libraryBooks = readNumericInputLine(src[index + 1]).sort(
+        (a, b) => bookPoints[b] - bookPoints[a]
+      );
+
+      const maxPoints = libraryBooks.reduce(
+        (acc, book) => acc + bookPoints[book],
+        0
+      );
+
+      const availableScanningTime = numOfDays - signUp;
+
+      return [
+        ...prev,
+        {
+          index: index / 2,
+          libraryBooks,
+          numOfLibraryBooks,
+          signUp,
+          shipCapacity,
+          maxPoints,
+          availableScanningTime
+        }
+      ];
+    }
+    return prev;
+  }, []);
+
+  return { numOfBooks, numOfLibraries, numOfDays, bookPoints, libraries };
+}
+
+function updateLibraries({ libraries, bookSet, daysLeft, bookPoints }) {
+  return libraries.map(library => {
+    const libraryBooks = library.libraryBooks.filter(
+      book => !bookSet.has(book)
+    );
+
+    const availableScanningTime = daysLeft - library.signUp;
+
+    const maxPoints = libraryBooks.reduce(
+      (acc, book) => acc + bookPoints[book],
+      0
+    );
+
+    return { ...library, libraryBooks, maxPoints, availableScanningTime };
+  });
+}
+
+function sortLibraries({ libraries }) {
+  const compareFn = (b, a) => {
+    const ret =
+      b.availableScanningTime * b.shipCapacity * b.maxPoints -
+      a.availableScanningTime * a.shipCapacity * a.maxPoints;
+    return ret;
+  };
+  return libraries.slice(0).sort(compareFn);
+}
+
+function addLibraryToOutput({ output, library, bookSet, daysLeft }) {
+  const qTyOfBooksToShip = Math.floor(daysLeft / library.shipCapacity);
+
+  const booksToShip = library.libraryBooks
+    .filter(e => !bookSet.has(e))
+    .slice(0, qTyOfBooksToShip);
+
+  if (booksToShip.length) {
+    // update the output
+    booksToShip.forEach(book => bookSet.add(book));
+    output.push({ ...library, booksToShip });
+  }
+}
+
+fs.readFile(path.resolve(__dirname, "..", file), "utf-8", (err, data) => {
+  if (err) return console.log("Read Error: ", err);
+
+  // save all scanned books to this set
+  const scannedBooks = new Set();
+
+  const {
+    numOfBooks,
+    numOfLibraries,
+    numOfDays,
+    bookPoints,
+    ...restOf
+  } = parseInputData(data);
+
+  let { libraries } = restOf;
+
+  // console.log({ numOfBooks, numOfLibraries, numOfDays, bookPoints, libraries });
+
+  let daysLeft = numOfDays;
+  let output = [];
+
+  libraries = sortLibraries({ libraries });
+
+  while (daysLeft >= 0) {
+    const [currLib, ...rest] = libraries;
+
+    if (!currLib) {
+      break;
+    }
+
+    // add library to output
+    addLibraryToOutput({
+      output,
+      library: currLib,
+      bookSet: scannedBooks,
+      daysLeft
+    });
+
+    // update library cycle
+    daysLeft = daysLeft - currLib.signUp;
+
+    libraries = updateLibraries({
+      libraries: rest,
+      bookSet: scannedBooks,
+      daysLeft,
+      bookPoints
+    });
+
+    libraries = sortLibraries({ libraries });
+  }
+
+  estimatePoints(output, bookPoints);
+
+  printOut(output);
 });
