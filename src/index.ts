6e1c1f4129bb8fd5f31dc461f1cc870ae7afcaa2
Joseph Chamochumbi <sephxd1234@gmail.com>
2022-02-24 09:37:08 +0100

    chore: Read from file and save to file

--- src/index.ts
+++ src/index.ts
@@ -1,85 +1,8 @@
-export {};
-
 const input = await Deno.readTextFile(`./input/${Deno.args[0]}.txt`);
-const rows = input.split("\n");
 
-const [TOTAL_TIME, INTERSECTIONS, STREETS, CARS, POINTS] = rows[0]
-  .split(" ")
-  .map(Number);
+console.log({ input });
 
-const streets = rows.slice(1, STREETS + 1).map((line) => {
-  const [B, E, name, time] = line.split(" ");
-  return { B: parseInt(B), E: parseInt(E), name, time: parseInt(time) };
-});
-
-const cars = rows.slice(STREETS + 1, CARS + STREETS + 1).map((line, id) => {
-  const [P, ...routes] = line.split(" ");
-  return { P: parseInt(P), routes, id };
-});
-
-const streetSet = new Set(cars.map(({ routes }) => routes).flat(Infinity));
-
-const streetCount = cars.reduce<Record<string, number>>(
-  (prev, { id, routes }) => {
-    routes.forEach((route, index) => {
-      prev[route] = prev[route] || 0;
-      const weight = routes
-        .slice(index + 1)
-        .reduce(
-          (prev, curr) =>
-            prev + streets.filter(({ name }) => name === curr)[0].time,
-          0
-        );
-      prev[route] = weight + prev[route];
-    });
-
-    return prev;
-  },
-  {}
-);
-
-// console.log(streetSet.size, STREETS, streetCount);
-
-// console.log(streets);
-// console.log(cars);
-
-const intersections = Array.from({ length: INTERSECTIONS }, (_, id) => {
-  const inputs = streets.filter(({ E }) => E === id);
-  return { id, inputs };
-});
-
-// console.log(intersections);
-
-const schedule = intersections.map(({ id, inputs }) => {
-  const validInputs = inputs.filter((input) => streetSet.has(input.name));
-  if (validInputs.length === 0) {
-    return null;
-  }
-
-  const total = validInputs.reduce(
-    (prev, { name }) => streetCount[name] + prev,
-    0
-  );
-
-  const weights = validInputs
-    .map(({ name }) => streetCount[name])
-    .sort((a, b) => a - b)
-    .map((x) => Math.ceil(x / (total || 1) || 1))
-    .reverse();
-
-  return [
-    id,
-    validInputs.length,
-    ...validInputs
-      .sort((a, b) => streetCount[a.name] - streetCount[b.name])
-      .map(({ name }, index) => {
-        return `${name} ${weights[index]}`;
-      })
-  ].join("\n");
-});
-
-const validSchedule = schedule.filter(Boolean);
-const answer = `${validSchedule.length}\n${validSchedule.join("\n")}`;
+const answer = `Echo`;
 
 // console.log(answer);
 
@@ -88,4 +11,4 @@ const write = Deno.writeTextFile(`./output/out_${Deno.args[0]}.txt`, answer);
 write.then(() => console.log(`Written to: ./output/out_${Deno.args[0]}.txt`));
 
 // from root of project:
-// deno run --allow-read --allow-write ./src/index.ts
+// deno run --allow-read --allow-write ./src/index.ts a
